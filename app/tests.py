edb073d0940daa8122cc790ccedc5f3344ff8341
Ritesh Patil <96098253+ritesh1603@users.noreply.github.com>
2024-07-08 17:27:57 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,34 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.http import HttpRequest
+from .views import hello, form
 
-# Create your tests here.
+class URLTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello_url(self):
+        response = self.client.get(reverse('hello'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'hello.html')
+
+    def test_form_url(self):
+        response = self.client.get(reverse('form'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'form.html')
+
+class ViewTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello_view(self):
+        request = HttpRequest()
+        response = hello(request)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'The only way to learn is to live!', response.content)
+
+    def test_form_view(self):
+        request = HttpRequest()
+        response = form(request)
+        self.assertEqual(response.status_code, 200)
+
