b0bf5b9a5dbb1ccc2c828069c799ef279b45a327
Ritesh Patil <96098253+ritesh1603@users.noreply.github.com>
2024-07-08 22:48:52 +0530

    Update tests.py

--- app/tests.py
+++ app/tests.py
@@ -1,34 +1,25 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import reverse, resolve
 from .views import hello, form
+        
 
-class URLTests(TestCase):
-    def setUp(self):
-        self.client = Client()
+class UrlsTestCase(TestCase):
+    def test_hello_url_is_resolved(self):
+        url = reverse('hello')
+        self.assertEqual(resolve(url).func, hello)
 
-    def test_hello_url(self):
+    def test_form_url_is_resolved(self):
+        url = reverse('form')
+        self.assertEqual(resolve(url).func, form)
+
+class ViewsTestCase(TestCase):
+    def test_hello_view(self):
         response = self.client.get(reverse('hello'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'hello.html')
 
-    def test_form_url(self):
+    def test_form_view(self):
         response = self.client.get(reverse('form'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'form.html')
-
-class ViewTests(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_hello_view(self):
-        request = HttpRequest()
-        response = hello(request)
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(b'The only way to learn is to live!', response.content)
-
-    def test_form_view(self):
-        request = HttpRequest()
-        response = form(request)
-        self.assertEqual(response.status_code, 200)
-
+ 
